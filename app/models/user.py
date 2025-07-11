5ab0d68f05366929a37360ecb4cb49c9ddcfbc0c
Sam Lim <sam@typhonic.io>
2025-07-11 15:22:15 -0700

    Define initial user model

---
+++ app/models/user.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.types import DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
+
+from app.database import Base
+
+
+class User(Base):
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    username = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+    is_superuser = Column(Boolean, default=False, nullable=False)
+
+    # invite_code = Column(String, ForeignKey("invites.code"))
+    # invite = relationship("Invite", back_populates="users")
+
+    # guilds = relationship("Guild", back_populates="members")
+
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now)
