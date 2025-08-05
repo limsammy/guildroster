d33a8837158c3b5478e385bd37709f2771566b0b
Sam Lim <sam@typhonic.io>
2025-08-05 08:08:27 -0700

    chore: bump version to 1.5.6

--- app/config.py
+++ app/config.py
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
     APP_DESCRIPTION: str = (
         "GuildRoster is a tool for managing your guild's roster and tracking attendance."
     )
-    VERSION: str = "1.5.5"
+    VERSION: str = "1.5.6"
     ENV: str = "dev"
     SECRET_KEY: str = "supersecret"
 
